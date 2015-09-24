6eb2ee053e5cbc4613cd981d3cc9933389027888
nazim <amin.nazim13@gmail.com>
2015-09-24 04:09:58 -0400

    window resize statement

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,12 @@ $(document).ready(function () {
 $(window).resize(function () {
     if (document.documentElement.clientWidth <= 620) {
         $('.col-xs-6').addClass('col-md-4').removeClass('col-xs-6');
-    } else if (document.documentElement.clientWidth <= 990) {} else {
-        $('.col-md-4').addClass('col-xs-6');
+    } else if (document.documentElement.clientWidth >= 620) {
+        $('.col-md-4').addClass('col-xs-6').removeClass('col-md-4');
+    }
+    if (document.documentElement.clientWidth >= 720) {
+        $('.col-xs-6').addClass('col-md-4');
+    } else {
+        $('.col-xs-6').addClass('col-md-4');
     }
 }).resize()

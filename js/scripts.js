749fcf100a4f38ab807ad124d42a73ace82f4d4c
nazim <amin.nazim13@gmail.com>
2015-09-24 04:01:33 -0400

    responsive cards

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ $(document).ready(function () {
         }).toggleClass('ndt2');
     }, 2000);
 });
+$(window).resize(function () {
+    if (document.documentElement.clientWidth <= 620) {
+        $('.col-xs-6').addClass('col-md-4').removeClass('col-xs-6');
+    } else if (document.documentElement.clientWidth <= 990) {} else {
+        $('.col-md-4').addClass('col-xs-6');
+    }
+}).resize()
